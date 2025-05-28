f111d41d3aef7a21692c43d34034e256dae766a3
neothenil <727549953@qq.com>
2025-05-28 23:58:12 +0800

    Chapter 4: Scanning

---
+++ src/Scanner.cpp
@@ -0,0 +1,142 @@
+#include "Scanner.h"
+#include "Lox.h"
+
+namespace lox {
+
+const std::map<std::string, TokenType> Scanner::sKeywords {
+    {"and", TokenType::AND},
+    {"class", TokenType::CLASS},
+    {"else", TokenType::ELSE},
+    {"false", TokenType::FALSE},
+    {"for", TokenType::FOR},
+    {"fun", TokenType::FUN},
+    {"if", TokenType::IF},
+    {"nil", TokenType::NIL},
+    {"or", TokenType::OR},
+    {"print", TokenType::PRINT},
+    {"return", TokenType::RETURN},
+    {"super", TokenType::SUPER},
+    {"this", TokenType::THIS},
+    {"true", TokenType::TRUE},
+    {"var", TokenType::VAR},
+    {"while", TokenType::WHILE}
+};
+
+std::vector<Token> Scanner::scanTokens()
+{
+    while (!isAtEnd()) {
+        mStart = mCurrent;
+        scanToken();
+    }
+    mTokens.push_back(Token(TokenType::TOKEN_EOF, "", std::any(), mLine));
+    return mTokens;
+}
+
+void Scanner::scanToken()
+{
+    char c = advance();
+    switch (c) {
+    // single character tokens
+    case '(': addToken(TokenType::LEFT_PAREN); break;
+    case ')': addToken(TokenType::RIGHT_PAREN); break;
+    case '{': addToken(TokenType::LEFT_BRACE); break;
+    case '}': addToken(TokenType::RIGHT_BRACE); break;
+    case ',': addToken(TokenType::COMMA); break;
+    case '.': addToken(TokenType::DOT); break;
+    case '-': addToken(TokenType::MINUS); break;
+    case '+': addToken(TokenType::PLUS); break;
+    case ';': addToken(TokenType::SEMICOLON); break;
+    case '*': addToken(TokenType::STAR); break;
+    // operators
+    case '!':
+        addToken(match('=') ? TokenType::BANG_EQUAL : TokenType::BANG);
+        break;
+    case '=':
+        addToken(match('=') ? TokenType::EQUAL_EQUAL : TokenType::EQUAL);
+        break;
+    case '<':
+        addToken(match('=') ? TokenType::LESS_EQUAL : TokenType::LESS);
+        break;
+    case '>':
+        addToken(match('=') ? TokenType::GREATER_EQUAL : TokenType::GREATER);
+        break;
+    case '/':
+        if (match('/')) {
+            // A comment goes until the end of line.
+            while (peek() != '\n' && !isAtEnd()) advance();
+        }
+        else {
+            addToken(TokenType::SLASH);
+        }
+        break;
+    // whitespaces
+    case ' ':
+    case '\r':
+    case '\t':
+        break;
+    case '\n':
+        ++mLine;
+        break;
+    case '"':
+        string();
+        break;
+    default:
+        if (isDigit(c)) {
+            number();
+        }
+        else if (isAlpha(c)) {
+            identifier();
+        }
+        else {
+            error(mLine, "Unexpected character.");
+        }
+        break;
+    }
+}
+
+void Scanner::string()
+{
+    while (peek() != '"' && !isAtEnd()) {
+        if (peek() == '\n') ++mLine;
+        advance();
+    }
+
+    if (isAtEnd()) {
+        error(mLine, "Unterminated string.");
+        return;
+    }
+
+    // The closing ".
+    advance();
+
+    // Trim the surrounding quotes.
+    std::string value = mSource.substr(mStart + 1, mCurrent - mStart - 2);
+    addToken(TokenType::STRING, value);
+}
+
+void Scanner::number()
+{
+    while (isDigit(peek())) {
+        advance();
+    }
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext())) {
+        // Consume the "."
+        advance();
+        while (isDigit(peek())) advance();
+    }
+    addToken(TokenType::NUMBER, std::stod(mSource.substr(mStart, mCurrent - mStart)));
+}
+
+void Scanner::identifier()
+{
+    while (isAlphaNumeric(peek())) advance();
+    std::string text = mSource.substr(mStart, mCurrent - mStart);
+    TokenType type = TokenType::IDENTIFIER;
+    if (sKeywords.find(text) != sKeywords.end()) {
+        type = sKeywords.at(text);
+    }
+    addToken(type);
+}
+
+}
--- src/Scanner.h
+++ src/Scanner.h
@@ -3,15 +3,59 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <any>
+#include <map>
+#include <magic_enum/magic_enum.hpp>
 
 namespace lox {
 
-using Token = std::pair<int, std::string>;
+enum class TokenType
+{
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    // EOF is defined as a macro expanded to (-1) by standard library.
+    // So we use TOKEN_EOF here.
+    TOKEN_EOF
+};
+
+class Token
+{
+public:
+    Token(TokenType type, const std::string& lexeme,
+        const std::any& literal, int line):
+        type(type), lexeme(lexeme), literal(literal), line(line)
+    {}
+
+private:
+    TokenType type;
+    std::string lexeme;
+    std::any literal;
+    int line;
+
+    friend std::ostream& operator<<(std::ostream&, const Token&);
+};
 
 inline std::ostream& operator<<(std::ostream& out, const Token& token)
 {
-    out << "type: " << token.first;
-    out << ", lexeme: " << token.second;
+    out << "type: " << magic_enum::enum_name(token.type);
+    out << ", lexeme: " << token.lexeme;
+    out << ", has_literal: "
+        << (token.literal.has_value() ? "true" : "false");
     return out;
 }
 
@@ -20,10 +64,56 @@ class Scanner
 public:
     Scanner(const std::string& source): mSource(source) {}
 
-    std::vector<Token> scanTokens() { return {}; }
+    std::vector<Token> scanTokens();
+
+private:
+    bool isAtEnd() const { return mCurrent >= mSource.length(); }
+    void scanToken();
+    char advance() { return mSource[mCurrent++]; }
+    void addToken(TokenType type) { addToken(type, std::any()); }
+    void addToken(TokenType type, const std::any& literal)
+    {
+        auto text = mSource.substr(mStart, mCurrent - mStart);
+        mTokens.push_back(Token(type, text, literal, mLine));
+    }
+    bool match(char expected)
+    {
+        if (isAtEnd()) return false;
+        if (mSource[mCurrent] != expected) return false;
+        ++mCurrent;
+        return true;
+    }
+    char peek() const
+    {
+        if (isAtEnd()) return '\0';
+        return mSource[mCurrent];
+    }
+    char peekNext() const
+    {
+        if (mCurrent + 1 >= mSource.length()) return '\0';
+        return mSource[mCurrent + 1];
+    }
+    bool isDigit(char c) const { return c >= '0' && c <= '9'; }
+    bool isAlpha(char c) const
+    {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+               c == '_';
+    }
+    bool isAlphaNumeric(char c) { return isAlpha(c) || isDigit(c); }
+
+    void string();
+    void number();
+    void identifier();
 
 private:
     std::string mSource;
+    std::vector<Token> mTokens;
+    size_t mStart = 0;
+    size_t mCurrent = 0;
+    size_t mLine = 1;
+
+    static const std::map<std::string, TokenType> sKeywords;
 };
 
 }
