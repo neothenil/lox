f095e555007d89ee5286d05efba96b63bdf37db3
neothenil <727549953@qq.com>
2025-07-28 00:37:12 +0800

    support recursive function

--- src/LoxCallable.cpp
+++ src/LoxCallable.cpp
@@ -9,13 +9,13 @@ int LoxFunction::arity()
 
 any LoxFunction::call(Interpreter* interpreter, std::vector<any>& arguments)
 {
-    auto environment = std::make_unique<Environment>(Interpreter::globals());
+    interpreter->environment = std::make_unique<Environment>(
+        std::move(interpreter->environment));
     for (int i = 0; i < declaration->params->size(); ++i) {
-        environment->define(declaration->params->at(i).lexeme, arguments.at(i));
+        interpreter->environment->define(
+            declaration->params->at(i).lexeme, arguments.at(i));
     }
-    std::swap(interpreter->environment, environment);
-    // make sure the environment will be restored even if exception is raised.
-    EnvironmentSwapGuard guard(interpreter->environment, environment);
+    EnvironmentGuard guard(interpreter->environment);
     interpreter->executeBlock(declaration->body.get());
     return any();
 }
